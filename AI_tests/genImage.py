3bc8e343d2d2168b56e5ac15569852b706efc0a9
jakowisp <ddilbeck@yahoo.com>
2025-03-29 21:02:01 -0700

    Update genImage.py to be pg rated

--- AI_tests/genImage.py
+++ AI_tests/genImage.py
@@ -3,7 +3,7 @@ import numpy as np
 pipeline = DiffusionPipeline.from_pretrained(
     "RunDiffusion/Juggernaut-X-v10", cache_dir="./cache"
 )
-prompt = "Naked 18yo. 5ft4in. 130lbs. 36C breasts. legs are spread open waiting to have a cock inserted"
+prompt = "Generate a cowgirl on a horse with red hat"
 
 # Set num_inference_steps as per your memory or computation
 image = pipeline(prompt, num_inference_steps=100, guidance_strength=0.7)
